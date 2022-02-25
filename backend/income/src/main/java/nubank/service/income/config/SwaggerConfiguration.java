5d7f1d6eed484c62300c8ba63bd3169b2a01623a
Humberto Dias <humberto.dias@ubisoft.com>
2022-02-25 19:21:32 +0100

    Skipping Profile for swagger

--- backend/income/src/main/java/nubank/service/income/config/SwaggerConfiguration.java
+++ backend/income/src/main/java/nubank/service/income/config/SwaggerConfiguration.java
@@ -7,7 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@Profile({"dev", "qa", "test"})
+//@Profile({"dev", "qa", "test"})
 public class SwaggerConfiguration implements WebMvcConfigurer
 {
 
