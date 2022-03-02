8613b29649406826f7fa98f64bd977a1d4c59cca
Humberto Dias <humberto.dias@ubisoft.com>
2022-03-02 14:33:29 +0100

    [LB] CORS

--- app/backend/account/src/main/java/nubank/service/account/config/CorsConfiguration.java
+++ app/backend/account/src/main/java/nubank/service/account/config/CorsConfiguration.java
@@ -1,12 +1,14 @@
 package nubank.service.account.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@Profile("cors")
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
