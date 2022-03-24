8103f5b6f79a05260eaa19d1272ab506db717b89
Humberto Dias <humberto.dias@ubisoft.com>
2022-03-24 01:55:10 +0100

    [VULNERABILITY] Removing Firebase credential file

--- app/backend/cm/src/main/java/bank/cm/App.java
+++ app/backend/cm/src/main/java/bank/cm/App.java
@@ -4,24 +4,35 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 public class App {
 
+	@Value("${FIREBASE_PROJECT_ID}")
+	String firebaseProjectId;
+
+	@Value("${FIREBASE_SERVICE_ACCOUNT_KEY}")
+	String firebaseServiceAccountKey;
+
 	@Bean
 	FirebaseMessaging firebaseMessaging() throws IOException {
+		InputStream fireBaseServiceAccountKeyStream = new ByteArrayInputStream(firebaseServiceAccountKey.getBytes(StandardCharsets.UTF_8));
 		GoogleCredentials googleCredentials = GoogleCredentials
-				.fromStream(new ClassPathResource("firebase-service-account.json").getInputStream());
+				.fromStream(fireBaseServiceAccountKeyStream);
 		FirebaseOptions firebaseOptions = FirebaseOptions
 				.builder()
 				.setCredentials(googleCredentials)
 				.build();
-		FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "bank-elastic-stack");
+		FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, firebaseProjectId);
 		return FirebaseMessaging.getInstance(app);
 	}
 
